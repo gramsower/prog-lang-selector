916fabd7af9369a8d90454b15d15284e67b801d5
gramsower <110431307+gramsower@users.noreply.github.com>
2022-08-26 22:03:53 -0700

    includes functional javascript (mostly debugged)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // UI Logic
 
 function hideResultsAndError() {
-  document.getElementById("error-message).setAttribute("class", "hidden");
+  document.getElementById("error-message").setAttribute("class", "hidden");
   document.getElementById("C#").setAttribute("class","hidden");
   document.getElementById("Java").setAttribute("class", "hidden");
   document.getElementById("HTML").setAttribute("class", "hidden");
@@ -11,34 +11,36 @@ function hideResultsAndError() {
 }
 
 window.onload = function() {
+  hideResultsAndError();
+
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideResultsAndError();
     const userName = document.querySelector("input#userName").value;
-    const firstCompAge = document.querySelector("input#firstCompAge");
+    const firstCompAge = document.querySelector("input#age");
     const userExp = parseInt(document.querySelector("input#experience").value);
     const looks = parseInt(document.querySelector("input#looks").value);
 
-    if (userName && firstCompAge && userExp && looks) {
-      if (userExp  <=1) {
+    if (userName && age && looks) {
+      if (userExp <= 1) {
         if (looks === 1) {
           document.getElementById("C#").removeAttribute("class");
         } else if (looks === 2) {
           document.getElementById("Java").removeAttribute("class");
         } else {
           document.getElementById("HTML").removeAttribute("class");
-        }
-      } else if {
+        };
+      } else {
         if (looks === 1) {
           document.getElementById("PHP").removeAttribute("class");
         } else if (looks === 2) {
           document.getElementById("React").removeAttribute("class");
         } else {
           document.getElementById("JavaScript").removeAttribute("class");
-        } 
-      } else {
-        document.getElementById("error-message").removeAttribute("class");
-      }
-    };
+        };
+      };
+    } else {
+      document.getElementById("error-message").removeAttribute("class");
+    }
   };
 };
