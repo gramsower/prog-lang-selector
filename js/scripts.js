74b10d1907bcf711a89c3e803077e5e87ac55c82
gramsower <110431307+gramsower@users.noreply.github.com>
2022-08-26 19:12:31 -0700

    WIP begin adding javascript to hide results, create variables for manipulation, and begin iff logic implementation.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// UI Logic
+
+function hideResultsAndError() {
+  document.getElementById("error-message).setAttribute("class", "hidden");
+  document.getElementById("C#").setAttribute("class","hidden");
+  document.getElementById("Java").setAttribute("class", "hidden");
+  document.getElementById("HTML").setAttribute("class", "hidden");
+  document.getElementById("PHP").setAttribute("class", "hidden");
+  document.getElementById("React").setAttribute("class", "hidden");
+  document.getElementById("JavaScript").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+  hideResultsAndError();
+  document.getElementById("
+
+  function handleRadio(event) {
+    event.preventDefault();
+    hideResultsAndError();
+    const radioSelection = document.querySelector("input[name='experience']:checked").value;
+    const looks = parseInt(document.querySelector("input#looks").value;
+  
+  if (radioSelection && looks) {
+    document.getElementById("
+
+
+
+
+  } else {
+    document.getElementById("error-message").remove.Attribute("class");
+    }
+  };
+}
